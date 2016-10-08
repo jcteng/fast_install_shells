7288fe94806fd9945f817dab02d15bbc012b2ba4
jcteng <jianchao.teng@outlook.com>
2016-10-09 02:53:23 +0800

    Changes to be committed: 	modified:   linux/cn_pip.sh 	modified:   linux/pt 	new file:   linux/py_lib_depends.sh

---
+++ linux/py_lib_depends.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y build-essential binutils-doc autoconf flex bison libjpeg-dev
+sudo apt-get install -y libfreetype6-dev zlib1g-dev libzmq3-dev libgdbm-dev libncurses5-dev
+sudo apt-get install -y automake libtool libffi-dev curl git tmux gettext
+sudo apt-get install libxml2-dev libxslt-dev
+
